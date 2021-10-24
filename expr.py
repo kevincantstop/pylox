a24cdf286ad0432a1b6c0b06bdeabddfb43947ab
Kevin Yang <kevincantstop@gmail.com>
2021-10-24 23:56:26 +0800

    added expr

---
+++ expr.py
@@ -0,0 +1,29 @@
+import abc
+
+
+@abc
+class Expr:
+    pass
+
+
+class Binary(Expr):
+    def __init__(self, left, operator, right):
+        self.left = left
+        self.operator = operator
+        self.right = right
+
+
+class Grouping(Expr):
+    def __init__(self, expr):
+        self.expr = expr
+
+
+class Literal(Expr):
+    def __init__(self, value):
+        self.value = value
+
+
+class Unary(Expr):
+    def __init__(self, operator, right):
+        self.operator = operator
+        self.right = right
